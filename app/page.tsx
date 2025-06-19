dd8e8cdde5ffec1dd9e1571dd1fc18b625c64851
felixjhonata <50239403+felixjhonata@users.noreply.github.com>
2025-06-19 17:38:28 +0700

    SCRUM-12: Add form validation for username and password in login component

--- app/page.tsx
+++ app/page.tsx
@@ -8,41 +8,67 @@ export default function Home() {
   const [passwordError, setPasswordError] = useState("")
   const [showPassword, setShowPassword] = useState(false)
 
-  const loginBtnClick = () => {
+  const validateUsername = (username: string) => {
     if (username === "") {
       setUsernameError("Username tidak boleh kosong")
+      return false
     } else {
       setUsernameError("")
+      return true
     }
+  }
 
+  const validatePassword = (password: string) => {
     if (password === "") {
       setPasswordError("Password tidak boleh kosong")
+      return false
     } else {
       setPasswordError("")
+      return true
     }
+  }
+
+  const validateForm = () => {
+    let hasError = !validateUsername(username)
+    hasError = !validatePassword(password) || hasError
+    return hasError
+  }
+
+  const loginBtnClick = () => {
+    if (!validateForm()) return
 
     // TODO: Call login authentication logic in server side (handled by controller)
+  }
+
+  const onUsernameChange = (username: string) => {
+    setUsername(username)
+    validateUsername(username)
+  }
+
+  const onPasswordChange = (password: string) => {
+    setPassword(password)
+    validatePassword(password)
   }
 
   const onShowPassword = () => {
     setShowPassword(!showPassword)
   }
- 
+
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <div className="flex flex-col border-2 p-8 rounded-xl min-w-sm">
         <img src="/omnikasir.svg" alt="Logo Omnikasir" className="flex flex-row justify-center items-center max-h-16 max-w-full" />
 
-        <hr className="my-8 border-1"/>
+        <hr className="my-8 border-1" />
 
         <h1 className="text-center font-bold text-2xl mb-2">Login</h1>
 
         <label htmlFor="username" className="mt-2 font-bold">Username</label>
-        <input type="text" name="username" id="username" className="border-2 p-2 rounded-lg my-1" placeholder="Username" autoComplete="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type="text" name="username" id="username" className="border-2 p-2 rounded-lg my-1" placeholder="Username" autoComplete="username" value={username} onChange={(e) => { onUsernameChange(e.target.value) }} />
         {usernameError !== "" && <p className="text-red-600">{usernameError}</p>}
 
         <label htmlFor="password" className="mt-3 font-bold">Password</label>
-        <input type={showPassword ? "text" : "password"} name="password" id="password" className="border-2 p-2 rounded-lg my-1" placeholder="Password" autoComplete="current-password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <input type={showPassword ? "text" : "password"} name="password" id="password" className="border-2 p-2 rounded-lg my-1" placeholder="Password" autoComplete="current-password" value={password} onChange={(e) => { onPasswordChange(e.target.value) }} />
         {passwordError !== "" && <p className="text-red-600">{passwordError}</p>}
 
         <span className="flex flex-row items-center mt-3">
@@ -50,7 +76,7 @@ export default function Home() {
           <label htmlFor="showPassword" className="ml-2">Show Password</label>
         </span>
 
-        <button className="mt-5 bg-black text-white p-2 rounded-xl max-w cursor-pointer" onClick={loginBtnClick}>Login</button>
+        <button className="mt-5 bg-black text-white p-2 rounded-xl max-w cursor-pointer disabled:bg-stone-300" onClick={loginBtnClick}>Login</button>
       </div>
     </div>
   );
