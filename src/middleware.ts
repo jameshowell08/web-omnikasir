87f5616de20eb665340657c2fc107138b84d886f
Felix Jhonata <felixjhonata3@gmail.com>
2025-07-06 14:42:16 +0700

    SCRUM-19: Moves login page to the designated path, added logic in middleware to redirect user to login page if they haven't logged in yet

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,13 +5,17 @@ export async function middleware(req: NextRequest) {
   const token = req.cookies.get("token")?.value
   const user = token ? await verifyJwt(token) : null
 
-  if (!user) {
+  const publicPaths = ["/", "/login"]
+
+  if (!publicPaths.includes(req.nextUrl.pathname) && !user) {
     return NextResponse.redirect(new URL("/login", req.url))
+  } else if (req.nextUrl.pathname === "/") {
+    return NextResponse.redirect(new URL("/dashboard", req.url))
   }
 
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*"], // Protect dashboard
+  matcher: ["/"], // Protect dashboard
 }
