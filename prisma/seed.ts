6a9156873ba6f3f081572d732881d50fe073e9f7
James Howell <howelljamescong@gmail.com>
2025-08-08 22:09:59 +0700

    SCRUM-25

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,20 +1,38 @@
 import { PrismaClient } from "@prisma/client"
-import bcrypt from "bcryptjs";
+import bcrypt from "bcryptjs"
+import fs from "fs"
+import path from "path"
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 async function main() {
-    const passwordHash = bcrypt.hashSync('admin123', 10);
-    const admin = await prisma.users.upsert({
-        where: { user_id: "US000" },
-        update: {},
-        create: {
-            user_id: "US000",
-            username: "admin",
-            password: passwordHash
-        }
-    })
-    console.log(admin)
+  //--users seed --
+  const passwordHash = bcrypt.hashSync("admin123", 10)
+  const admin = await prisma.users.upsert({
+    where: { user_id: "US000" },
+    update: {},
+    create: {
+      user_id: "US000",
+      username: "admin",
+      password: passwordHash,
+    },
+  })
+  //--store seed-
+  const imagePath = path.join(__dirname, "assets", "walpaper.png")
+
+  const imageBuffer = fs.readFileSync(imagePath) // Load image as buffer
+
+  const store = await prisma.store.upsert({
+    where: { id: "dummy-store-1" },
+    update: {},
+    create: {
+      id: "dummy-store-1", // use any fixed UUID string or remove this to let it autogenerate
+      nama: "Toko Dummy",
+      alamat: "Jl. Contoh No. 123",
+      noHp: "081234567890",
+      profilePicture: imageBuffer,
+    },
+  })
 }
 
 main()
